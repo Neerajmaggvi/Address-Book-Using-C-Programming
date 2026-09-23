464fbdb71d956cc5a6213620e048ba4edc2aac1d
Neeraj Maggvi <neerajmaggavi09@gmail.com>
2026-09-23 23:00:20 +0530

    Added

--- contact.c
+++ contact.c
@@ -116,7 +116,7 @@ void createContact(AddressBook *addressBook)
             continue;
         }
 
-    
+        break;
     }
 
     if(attempts == 3)
@@ -127,6 +127,85 @@ void createContact(AddressBook *addressBook)
     
     // After validation copy the name into the structure.
     strcpy(addressBook->contacts[addressBook->contactCount].name, name);
+
+
+
+    // ---------------------- PHONE NUMBER VALIDATION ----------------------
+
+    char phone[20];
+    int phone_attempts = 0;
+
+    while (phone_attempts < 3)
+    {
+        printf("Enter the phone number: ");
+        scanf("%s", phone);
+
+        // Check phone number length
+        int len = strlen(phone);
+
+        if (len != 10)
+        {
+            printf("Invalid Phone number! Enter exactly 10 digits.\n");
+            phone_attempts++;
+            continue;
+        }
+
+        // Check first digit
+        if (phone[0] != '6' && phone[0] != '9')
+        {
+            printf("Invalid Phone number! Phone number must start from 6 or 9.\n");
+            phone_attempts++;
+            continue;
+        }
+
+        // Check whether all characters are digits
+        int valid = 1;
+
+        for (int i = 0; phone[i] != '\0'; i++)
+        {
+            if (!isdigit(phone[i]))
+            {
+                valid = 0;
+                break;
+            }
+        }
+
+        if (!valid)
+        {
+            printf("Invalid Phone number! Number must contain only digits.\n");
+            phone_attempts++;
+            continue;
+        }
+
+        // Find the duplicate number exists or not
+        int isduplicate = 0;
+        for (int i = 0; i < addressBook->contactCount; i++)
+        {
+            if (strcmp(phone, addressBook->contacts[i].phone) == 0)
+            {
+                isduplicate = 1;  
+                break; 
+            }
+        }
+
+        if(isduplicate)
+        {
+            printf("Invalid Phone number! There is a duplicate.\n");
+            phone_attempts++;
+            continue;
+        }
+
+        break;
+    }
+
+    if (phone_attempts == 3)
+    {
+        printf("Maximum attempts reached. Program terminated.\n");
+        return;
+    }
+
+
+
 }
 
 void searchContact(AddressBook *addressBook) 
